72de5cfe0cdb320b929a092b85c430e22bf196b2
teammdr <141670764+teammdr1@users.noreply.github.com>
2026-07-12 12:16:48 +0200

    Update creebackup.js

--- src/commands/backup/creebackup.js
+++ src/commands/backup/creebackup.js
@@ -1,26 +1,32 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const config = require('../../../config.js');
+const backup = require('discord-backup');
 
 module.exports = {
     name: 'creebackup',
     description: 'Crée une sauvegarde du serveur',
+
     async execute(client, message, args) {
         if (message.author.id !== config.ownerId) {
             return message.reply("Tu n'as pas la permission d'utiliser cette commande.");
         }
+
         try {
-            const backup = await client.api.guilds(message.guild.id).backups.post();
-        const embed = new EmbedBuilder()
-            .setTitle('Sauvegarde créée !')
-            .setDescription(`ID de la sauvegarde : \`${backup.id}\``)
-            .setColor('#00f000')
-            .setFooter({ text: config.footer })
-            .setTimestamp();
-        message.channel.send({ embeds: [embed] });
-    }
-        catch (err) {
+            const backupData = await backup.create(message.guild, {
+                jsonBeautify: true
+            });
+
+            const embed = new EmbedBuilder()
+                .setTitle('Sauvegarde créée !')
+                .setDescription(`ID de la sauvegarde : \`${backupData.id}\``)
+                .setColor('#00f000')
+                .setTimestamp();
+
+            await message.channel.send({ embeds: [embed] });
+
+        } catch (err) {
             console.error(err);
             return message.reply("Erreur lors de la création de la sauvegarde.");
         }
-}
+    }
 };
