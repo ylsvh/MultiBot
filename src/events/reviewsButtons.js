c251ec5417eae28a1f8ce72c715b777cb0c0409d
Silence <141670764+ylsvh@users.noreply.github.com>
2026-08-20 12:28:03 +0200

    Update reviewsButtons.js

--- src/events/reviewsButtons.js
+++ src/events/reviewsButtons.js
@@ -6,18 +6,12 @@ const DATA_PATH = path.join(__dirname, '../data/reviews.json');
 module.exports = {
   name: 'interactionCreate',
   once: false,
-
   async execute(interaction) {
-
-    // On filtre UNIQUEMENT les boutons
     if (!interaction.isButton()) return;
-
-    // On filtre UNIQUEMENT les boutons review
     if (!interaction.customId.startsWith('review_delete_')) return;
-
+    
     const userId = interaction.customId.split('_')[2];
-
-    // Permission
+    
     if (
       interaction.user.id !== userId &&
       !interaction.member.permissions.has('ManageMessages')
@@ -27,17 +21,12 @@ module.exports = {
         ephemeral: true
       });
     }
-
-    // Charger JSON
     let data = {};
     if (fs.existsSync(DATA_PATH)) {
       data = JSON.parse(fs.readFileSync(DATA_PATH));
     }
-
-    // Supprimer
     delete data[userId];
     fs.writeFileSync(DATA_PATH, JSON.stringify(data, null, 2));
-
     await interaction.message.delete();
   }
 };
