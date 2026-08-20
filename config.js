b5f125a0b7d515ae085b22c1ae178843f2b4f11b
Silence <141670764+ylsvh@users.noreply.github.com>
2026-08-20 12:18:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   token: "VOTRE_TOKEN_DISCORD",
   prefix: "+",
+  clientId: "ID_DE_VOTRE_BOT",
   embedColor: "#49ff02",
   ownerId: "VOTRE_ID_DISCORD",
   supportServerInvite: "https://discord.gg/votre-serveur",
