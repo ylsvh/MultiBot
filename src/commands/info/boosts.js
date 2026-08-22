0bd5086667bcf203b8533643a1fa79e2b32b9ea1
Silence <141670764+ylsvh@users.noreply.github.com>
2026-08-22 11:54:49 +0200

    Delete src/commands/info directory

--- src/commands/info/boosts.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "boosts",
-    description: "Afficher le nombre de boosts du serveur",
-    async execute(client, message, args) {
-        await message.channel.sendTyping();
-        const boosts = message.guild.premiumSubscriptionCount;
-        await message.channel.send(`Ce serveur a actuellement **${boosts}** boost(s).`);
-    }
-};
--- src/commands/info/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: 'Répond Pong !',
-  async execute(client, message, args) {
-        await message.channel.sendTyping();
-    message.reply('🏓 Pong !');
-  }
-};
